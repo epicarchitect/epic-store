fc2c7291001d190ad096fb051a3662473b752b6a
Alexander Kolmachikhin <akolmachikhin@icerockdev.com>
2022-08-29 04:27:22 +0700

    update

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -46,9 +46,9 @@ android {
 dependencies {
 //    implementation(project(":epic-store-navigation-compose"))
 //    implementation("com.github.jitpack.gradle-modular:server:1.3")
-    implementation("com.github.epicarchitect.epic-store:core:1.0.2")
-    implementation("com.github.epicarchitect.epic-store:compose:1.0.2")
-    implementation("com.github.epicarchitect.epic-store:google-navigation-compose:1.0.2")
+    implementation("com.github.epicarchitect.epic-store:core:1.0.3")
+    implementation("com.github.epicarchitect.epic-store:compose:1.0.3")
+    implementation("com.github.epicarchitect.epic-store:google-navigation-compose:1.0.3")
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.activity:activity-compose:1.5.1")
     implementation("androidx.compose.material:material:1.2.1")
