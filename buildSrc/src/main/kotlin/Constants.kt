a9e00f043d7d726bec3444c5caaaa8cbf1025af5
Alexander Kolmachikhin <akolmachikhin@icerockdev.com>
2022-09-18 02:42:36 +0700

    update

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,5 +3,5 @@ object Constants {
     const val COMPILE_SDK = 33
     const val COMPOSE_COMPILER_VERSION = "1.3.0"
     const val EPICARCHITECT_GROUP_NAME = "com.github.epicarchitect"
-    const val EPIC_STORE_VERSION = "1.0.4"
+    const val EPIC_STORE_VERSION = "1.0.5"
 }
